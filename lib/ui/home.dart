1bafc43027c5852302bfbcf61e21e400966ddb51
Didi Baka <ddiderson@gmail.com>
2021-08-11 11:21:06 +0200

    added custom button

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -4,16 +4,65 @@ import 'package:flutter/material.dart';
 class ScaffoldExample extends StatelessWidget {
   const ScaffoldExample({Key? key}) : super(key: key);
 
+  _tappedButton() {
+    debugPrint("Button function tapped");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Scaffold"),
+        title: Text("Didi Scaffold"),
         centerTitle: true,
+        backgroundColor: Colors.amberAccent.shade700,
+        actions: <Widget> [
+          IconButton(onPressed: () => debugPrint("Email Tapped"), icon: Icon(Icons.mail)),
+          IconButton(onPressed: _tappedButton, icon: Icon(Icons.alarm))
+        ],
+      ),
+      backgroundColor: Colors.lightBlueAccent,
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CustomButton()
+            // InkWell(
+            //   child: Text("Tap me!",
+            //   style: TextStyle(fontSize: 24.4),
+            //   ),
+            //   onTap: () => debugPrint("tapped"),
+            // )
+          ],
+
+        ),
       ),
     );
   }
 }
+
+class CustomButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        final snackBar = SnackBar(content: Text("Hi Didi Again"),
+        backgroundColor: Colors.amberAccent.shade700,
+        );
+        Scaffold.of(context).showSnackBar(snackBar);
+      },
+      child: Container(
+        padding: EdgeInsets.all(10.0),
+        decoration: BoxDecoration(
+          color: Colors.pinkAccent,
+          borderRadius: BorderRadius.circular(8.0)
+        ),
+        child: Text("Button"),
+      ),
+    );
+  }
+}
+
 
 
 class Home extends StatelessWidget {
